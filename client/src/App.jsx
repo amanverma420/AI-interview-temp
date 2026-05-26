40e7ff0ac21c2399023f8ce93387e44cdf00c1a4
amanverma420 <72482906+amanverma420@users.noreply.github.com>
2026-05-26 21:53:31 +0530

    production url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import InterviewHistory from './pages/InterviewHistory'
 import Pricing from './pages/Pricing'
 import InterviewReport from './pages/InterviewReport'
 
-export const ServerUrl  = "https://ai-interview-temp.onrender.com"
+export const ServerUrl  = import.meta.env.VITE_API_URL
 
 function App() {
 
